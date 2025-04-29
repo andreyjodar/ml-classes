53dbbfb65607ef8cc492aef5edfb91f67ace59e4
unknown <andreyviniciusjodar@gmail.com>
2025-04-29 19:12:28 -0300

    fixes

--- dataframe.py
+++ dataframe.py
@@ -6,12 +6,10 @@ last_column = dataframe.iloc[:, -1]
 occurrences = last_column.value_counts()
 print(occurrences)
 
-# find max distance for joao
-dataframe_joao = dataframe[dataframe['classificacao'] == 1]
-max_distance = dataframe_joao['distancia'].max()
-print(f'Maior Distância para João: {max_distance}')
+list_d = list(dataframe['desceu'])
+list_c = list(dataframe['classificacao'])
 
-# find max depth for maria
-dataframe_maria = dataframe[dataframe['classificacao'] == 2]
-max_depth = dataframe_maria['desceu'].max()
-print(f'Maior Profundidade para Maria: {max_depth}')
+count = [0, 0]
+
+for des,cls in zip(list_d, list_c):
+    pass
